515aa4a4cea7dc8b5ac1fd9024b3a0186ee9ccc8
dmytro_pietukhov <stalinsssr09@gmail.com>
2024-05-01 13:08:01 +0200

    classwork3

--- javastart/lecture1/classwork/ex1/Conditions.java
+++ javastart/lecture1/classwork/ex1/Conditions.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class Conditions {
     public static void main(String[] args) {
-        String login = "Root";
+        String login1 = "Root";
+        String login2 = "Admin";
         String password = "root";
 
         System.out.println("Enter Your Login!");
         Scanner scan = new Scanner(System.in);
 
         String userLogin = scan.next();
-        if (userLogin.equals(login)) {
+        if (userLogin.equals(login1)) {
             System.out.println("Enter Your Password!");
             Scanner scann = new Scanner(System.in);
             String userPassword = scann.next();
@@ -22,8 +23,14 @@ public class Conditions {
                 System.out.println("Wrong Password");
             }
         }
-        else {
-            System.out.println("Wrong login");
-        }
+        else if (userLogin.equals(login2)){
+            System.out.println("Enter Your Password!");
+            Scanner scann = new Scanner(System.in);
+            String userPassword = scann.next();
+            if (userPassword.equals(password)){
+                System.out.println("Welcome");
+            }
+            else System.out.println("Wrong Password");}
+        else System.out.println("Wrong login");
     }
 }
